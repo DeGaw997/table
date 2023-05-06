19d050eecb328db268eeb797d7d57ab31b799688
Damian <d.g24@interia.pl>
2023-05-06 18:39:18 +0200

    working version of table

--- New folder/style.js
+++ New folder/style.js
@@ -11,22 +11,21 @@ fetch('http://' + ip).then(res => res.json())
         
         for(let i = 0; i < info.length; i++) {
             let table1 =  document.createElement('tr')
+            let anotherTd =  document.createElement('td')
 
-            for(let i =0; i < info.length; i++) {
+            // for(let i =0; i < info.length; i++) {
 
-             let anotherTd =  document.createElement('td')
-             table1.append(anotherTd)
-            }
-            // let newTd = document.createElement('td') 
-            // let newTd2 = document.createElement('td') 
-            // let newTd3 = document.createElement('td') 
+            //  table1.append(anotherTd)
+            // }
+            let newTd = document.createElement('td') 
+            let newTd2 = document.createElement('td') 
+            let newTd3 = document.createElement('td') 
             tbody.append(table1)
             table1.append(newTd,newTd2,newTd3)
             newTd.append(info[i].brand)
             newTd2.append(info[i].year)
             newTd3.append(info[i].color)
            
-        }
 
 
 
@@ -36,5 +35,6 @@ fetch('http://' + ip).then(res => res.json())
         console.log(brandCarBmw);
         console.log(data)
     }
+}
 )
 
