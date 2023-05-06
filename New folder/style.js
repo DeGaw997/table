d3ea3424ad5c8331df3208fad88d3a92a4582f13
Damian <d.g24@interia.pl>
2023-05-06 18:37:13 +0200

    wersja1tabele

---
+++ New folder/style.js
@@ -0,0 +1,40 @@
+
+let tbody = document.querySelector('tbody')
+
+let ip = '192.168.1.5'
+
+fetch('http://' + ip).then(res => res.json())
+.then(
+    data => {
+        let info = data
+        
+        
+        for(let i = 0; i < info.length; i++) {
+            let table1 =  document.createElement('tr')
+
+            for(let i =0; i < info.length; i++) {
+
+             let anotherTd =  document.createElement('td')
+             table1.append(anotherTd)
+            }
+            // let newTd = document.createElement('td') 
+            // let newTd2 = document.createElement('td') 
+            // let newTd3 = document.createElement('td') 
+            tbody.append(table1)
+            table1.append(newTd,newTd2,newTd3)
+            newTd.append(info[i].brand)
+            newTd2.append(info[i].year)
+            newTd3.append(info[i].color)
+           
+        }
+
+
+
+        newSlot.append(brandCarBmw)
+        newSlot2.append(yearCarBmw)
+
+        console.log(brandCarBmw);
+        console.log(data)
+    }
+)
+
